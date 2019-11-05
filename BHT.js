06459900eaadd7c129287bf25bda6239560922e1
Space-G <gabriel.spaceg@gmail.com>
2019-11-05 18:58:24 -0300

    BHT funcionando

--- BHT.js
+++ BHT.js
@@ -1,30 +1,37 @@
 class BHT{
     constructor(m,n){
         this.m = m;
-        this.historicoTableSize = 2 ** m;
-        this.historico = new Array(this.historicoTableSize).fill(0);
+        this.historicoTableSize = Math.pow(2, m);
+        this.historico = new Array(this.historicoTableSize);
+        this.historico.fill(0);
         this.n = n;
-        this.counterSize = 2 ** n;
+        this.counterSize = Math.pow(2, n);
         this.counter = new Array(this.counterSize).fill(2);
         this.hits = 0;
         this.miss = 0;
     }
 
     doTheThing(endereco, resultado){
-        let histIndex = (parseInt(endereco, 16) >>> 2) % m;
-        let predicao = this.counter[this.historico[histIndex]];
+        var histIndex = (parseInt(endereco, 16) >>> 2) % this.historicoTableSize;
+        var predicao = this.counter[this.historico[histIndex]];
 
-        if ((predicao <= 1 && resultado == 'N') || (predicao >= 2 && resultado == 'T')){
+        if ((predicao <= 1 && resultado == "N") || (predicao >= 2 && resultado == "T")){
             this.hits += 1;
         } else {
             this.miss += 1;
         }
 
-        if (resultado == 'T'){
-            this.counter[this.historico[histIndex]] += (predicao < 4) ? 1 : 0;
-            this.historico[histIndex] = (this.historico[histIndex] >>> 1) + (2 ** (n - 1));
+        var posCounter = this.historico[histIndex];
+
+        if (resultado == "T"){
+            if (predicao < 3){
+                this.counter[posCounter]++;
+            }
+            this.historico[histIndex] = (this.historico[histIndex] >>> 1) + Math.pow(2, (this.n - 1));
         } else {
-            this.counter[this.historico[histIndex]] -= (predicao > 0) ? 1 : 0;
+            if (predicao > 0){
+                this.counter[posCounter]--;
+            }
             this.historico[histIndex] = (this.historico[histIndex] >>> 1);
         }
     }
@@ -49,13 +56,17 @@ var done = false;
 
 document.getElementById('file').onchange = function(){
     var file = this.files[0];
-  
+    bht = new BHT(getElementById("m").value, getElementById("n").value);
+
     var reader = new FileReader();
     reader.onload = function(){
         lineNumber = -1;
         fileAsString = this.result.split('\n');
     };
     reader.readAsText(file);
+
+
+    
 }
 
 function computeLine(){ // não chamem isso, front!!!!!!!
